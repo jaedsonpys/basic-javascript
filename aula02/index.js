49a5a260081eebe0cd3d8142301a81896b71511b
Jaedson Silva <contacanal798@gmail.com>
2022-07-13 13:28:59 -0300

    adding aula02

---
+++ aula02/index.js
@@ -0,0 +1,23 @@
+// tipos primitivos
+
+// boolean
+var bool = true;
+console.log(typeof(bool));
+
+// number
+var number = 15;
+console.log(typeof(number));
+
+// string
+var string = "Jaedson";
+console.log(typeof(string));
+
+// comparação
+
+// compara apenas o valor
+var compOne = "0" == 0;
+console.log(compOne);
+
+// compara o valor e o tipo
+var compTwo = "0" == 0;
+console.log(compTwo);
